3935f8aa9deee4d8dd716cfcd3e830cb051fbf4c
TulsiGupta <tulsigupta598@gmail.com>
2023-10-17 12:07:36 +0530

    commit5

--- SiteBackEnd/Controllers/UserController.js
+++ SiteBackEnd/Controllers/UserController.js
@@ -1,17 +1,33 @@
 import userModel from '../Model/UserModel.js'
+import validator from 'validator'
+import bcrypt from 'bcrypt'
 
 // Register controller-
 export const registerController = async(req,res)=>{
     try {
-        const{name,email,password} = req.body;
-        const user = new userModel({name,email,password, avtar: {
+        const{user,email,password} = req.body;
+        if(!user || !email || !password){
+            return res.status(400).send({
+                success:false,
+                message:"Please fill all details"
+            })
+        }
+        if(!validator.isEmail){
+            return res.status(400).send({
+                success:false,
+                message:"Please enter a valid email id"
+            })
+        }
+        // Hashing Password-
+        const hashedPassword = await bcrypt.hash(password,10)
+        const userDetails = new userModel({user,email,password:hashedPassword, avtar: {
             public_id: "Sample",
             url:"Sample",
           }}).save();
         return res.status(200).send({
             success:true,
             message:"User created successfully",
-            user
+            userDetails
         })
     } catch (error) {
         console.log(error);
