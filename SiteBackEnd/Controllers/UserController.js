1c5a0a394b766bb1725869513c7a5d95a0c41ec7
TulsiGupta <tulsigupta598@gmail.com>
2023-10-19 18:40:45 +0530

    redux toolkit

--- SiteBackEnd/Controllers/UserController.js
+++ SiteBackEnd/Controllers/UserController.js
@@ -1,6 +1,7 @@
 import userModel from '../Model/UserModel.js'
 import validator from 'validator'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 // Register controller-
 export const registerController = async(req,res)=>{
@@ -10,6 +11,13 @@ export const registerController = async(req,res)=>{
             return res.status(400).send({
                 success:false,
                 message:"Please fill all details"
+            })
+        }
+        const existingUser = await userModel.findOne({email})
+        if(existingUser){
+            return res.status(400).send({
+                success:false,
+                message:"Already registered. Please login."
             })
         }
         if(!validator.isEmail){
@@ -37,3 +45,66 @@ export const registerController = async(req,res)=>{
         })
     }
 }
+
+// Login controller-
+export const loginController = async(req,res)=>{
+    try {
+        const {email,password} = req.body;
+        // Validation-
+       if(!email || !password){
+        return res.status(400).send({
+            success:false,
+            message:"Please enter email and password"
+        })
+       }
+       //Email validation-
+       const user = await userModel.findOne({email}).select("+password")
+       const isPasswordMatch = await bcrypt.compare(password,user.password)
+       if(!user || !isPasswordMatch){
+        return res.status(400).send({
+            success:false,
+            message:"Invalid login credentials"
+        })
+       }
+       //Generating JWT Token-
+       const token = jwt.sign({id:user._id},process.env.JWT_SECRET_KEY,{expiresIn:"10d"})
+       res.cookie("jwtToken",token,{
+        httpOnly:true
+       })
+       res.status(200).send({
+        success:true,
+        message:"User login successfully",
+        user:{
+            email:user.email,
+            password:user.password
+        },
+        token
+       })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:"Something went wrong"
+        })
+    }
+}
+
+// Get single user details-
+// Protection through admin verification-
+// export const isAdmin = async(req,res) => {
+//     try{
+//      const user = await userModel.findById(req.user._id);
+//      if(user.role !== 1){
+//         return res.status(401).send({
+//             success:false,
+//             message:"UnAuthorized Access"
+//         });
+//      }
+//     }catch(error){
+//         console.log(error);
+//         res.status(401).send({
+//             success:false,
+//             message:"Error in Admin middleware"
+//         })
+//     }
+// }
--- SiteBackEnd/Routes/UserRoutes.js
+++ SiteBackEnd/Routes/UserRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 const router = express.Router();
-import {registerController} from '../Controllers/UserController.js'
+import {registerController,loginController} from '../Controllers/UserController.js'
 
 // Register user-
 router.post('/register',registerController)
 
 // Login user-
-
+router.post('/login',loginController)
 
 export default router
